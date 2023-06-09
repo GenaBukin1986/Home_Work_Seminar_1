207b802a4bf20ad17d34f33302cf588db61690f4
Nick_Bear <bear_76@mail.ru>
2023-06-09 21:30:04 +0300

    Решена задача с трехзначным числом

---
+++ task_5/Program.cs
@@ -0,0 +1,8 @@
+﻿// Задача 7. Выведите последнюю цифру трехзначного числа
+// 567 -> 7
+// 432 -> 2
+Console.Clear();
+Console.Write("Введите трехзначное число: ");
+int num = int.Parse(Console.ReadLine());
+if (num < 100 || num > 999) Console.Write("Вы ввели не трехзначное число!");
+else Console.Write(num % 10);
